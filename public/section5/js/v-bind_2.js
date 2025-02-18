f69640c5aaa0c8e421f62badec6c3245ee8af9d6
RyotaIhara <ihara0531@gail.com>
2025-02-18 19:35:36 +0900

    section5 complete

---
+++ public/section5/js/v-bind_2.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data: () => ({
+        message: 'Hello <span style="color:red">Vue.js</span>'
+    }),
+    methods: {
+        clickHandler: function(event) {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+app.mount("#v_bind_practice")
